6eb2f0cdff4b9b0a4a3f7dadb731cfdd8236d780
Dmitry Belyaev <didima220@mail.ru>
2017-05-11 03:23:19 +0300

    Add user background image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,6 +59,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation,
-                                 :name, :username, :avatar_url, :background_color)
+                                 :name, :username, :avatar_url, :background_color, :background_image)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509061233) do
+ActiveRecord::Schema.define(version: 20170511001532) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "text"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170509061233) do
     t.string   "password_salt"
     t.string   "avatar_url"
     t.string   "background_color", default: "51A0AF"
+    t.string   "background_image"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
